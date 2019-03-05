bdffcd047345839c7c298be4213d7d61d4ba493a
Andy P <amp205@medschl.cam.ac.uk>
2019-03-05 12:38:33 +0000

    Restructured and added code to pupulate sample data

---
+++ api/local/database-view-sql/database-views.py
@@ -0,0 +1,31 @@
+#
+#   Thiscovery API - THIS Institute’s citizen science platform
+#   Copyright (C) 2019 THIS Institute
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   A copy of the GNU Affero General Public License is available in the
+#   docs folder of this project.  It is also available www.gnu.org/licenses/
+#
+
+from api.common.pg_utilities import execute_query, execute_non_query, run_sql_script_file
+
+VIEW_SQL_FOLDER = './database-view-sql/'
+
+
+def create_all_views():
+    run_sql_script_file(VIEW_SQL_FOLDER + 'view_project_group_users_create.sql', None)
+    run_sql_script_file(VIEW_SQL_FOLDER + 'view_project_testgroup_users_create.sql', None)
+    run_sql_script_file(VIEW_SQL_FOLDER + 'view_projecttask_group_users_create.sql', None)
+    run_sql_script_file(VIEW_SQL_FOLDER + 'view_projecttask_testgroup_users_create.sql', None)
+
+if __name__ == "__main__":
+    create_all_views()
