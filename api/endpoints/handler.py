67e889df9564c50496a1cb500498d4598e17dac5
Andy P <amp205@medschl.cam.ac.uk>
2019-03-26 12:48:11 +0000

    Removed ping from handler.py

--- api/endpoints/handler.py
+++ api/endpoints/handler.py
@@ -21,38 +21,6 @@ import os
 from http import HTTPStatus
 from api.common.utilities import get_logger, get_correlation_id, get_secret, get_start_time, get_elapsed_ms
 
-
-def ping(event, context):
-    start_time = get_start_time()
-    logger = get_logger()
-
-    region = ''
-    aws = ''
-
-    try:
-        region = os.environ['AWS_REGION']
-        aws = os.environ['AWS_EXECUTION_ENV']
-    except:
-        pass
-
-    body = {
-        "message": "Response from THIS Institute citizen science API",
-        "region": region,
-        "aws":aws,
-        "input": event
-    }
-
-    response = {
-        "statusCode": HTTPStatus.OK,
-        "body": json.dumps(body)
-    }
-
-    correlation_id = get_correlation_id(event)
-    logger.info('API call', extra={'correlation_id': correlation_id, 'event': event, 'elapsed_ms': get_elapsed_ms(start_time)})
-
-    return response
-
-
 def hubspot_entity_info(event, context):
 
     body = {
@@ -92,6 +60,5 @@ def connection_info(event, context):
 
 
 if __name__ == "__main__":
-    result = ping(None, None)
-    # result = connection_info(None, None)
+    result = connection_info(None, None)
     print(result)
