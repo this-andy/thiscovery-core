730e1ed7894e0c4ec0863e090b78f3606762b7bf
André Sartori <afs25@cam.ac.uk>
2019-11-26 10:42:26 +0000

    #2836 comment: fixed param order in test_hubspot.py and test_mist.py; made expected_body DRY in test01 of test_project.py

--- api/tests/test_scripts/test_project.py
+++ api/tests/test_scripts/test_project.py
@@ -21,6 +21,7 @@ from http import HTTPStatus
 from unittest import TestCase
 from api.common.pg_utilities import insert_data_from_csv, truncate_table
 from api.common.utilities import set_running_unit_tests
+from api.common.dev_config import TIMEZONE_IS_BST
 from api.tests.test_scripts.testing_utilities import test_get, test_post, test_patch
 
 TEST_SQL_FOLDER = '../test_sql/'
@@ -36,6 +37,12 @@ class TestProject(TestCase):
     def setUpClass(cls):
         cls.maxDiff = None
         set_running_unit_tests(True)
+
+        truncate_table('public.projects_projecttask')
+        truncate_table('public.projects_externalsystem')
+        truncate_table('public.projects_tasktype')
+        truncate_table('public.projects_project')
+        truncate_table('public.projects_usergroup')
 
         insert_data_from_csv(TEST_DATA_FOLDER + 'usergroup_data.csv', 'public.projects_usergroup')
         insert_data_from_csv(TEST_DATA_FOLDER + 'project_data.csv', 'public.projects_project')
@@ -60,70 +67,69 @@ class TestProject(TestCase):
         from api.endpoints.project import list_projects_api
 
         expected_status = HTTPStatus.OK
-        # todo figure out how do do this properly!
-        expected_body_bst = [
-            {"id": "3ffc498f-8add-4448-b452-4fc7f463aa21", "name": "CTG Monitoring", "short_name": "CTG Monitoring",
-             "created": "2018-08-17T13:10:56.084487+01:00", "modified": "2018-08-17T13:10:56.119612+01:00", "status": "complete",
+
+        if TIMEZONE_IS_BST:
+            tz_hour = "13"
+            tz_offset = "01:00"
+            tz_closing_date_hour = "10"
+        else:
+            tz_hour = "12"
+            tz_offset = "00:00"
+            tz_closing_date_hour = "09"
+
+        expected_body = [
+            {"id": "3ffc498f-8add-4448-b452-4fc7f463aa21",
+             "name": "CTG Monitoring",
+             "short_name": "CTG Monitoring",
+             "created": f"2018-08-17T{tz_hour}:10:56.084487+{tz_offset}",
+             "modified": f"2018-08-17T{tz_hour}:10:56.119612+{tz_offset}",
+             "status": "complete",
              "visibility":"public",
              "tasks": [
-                 {"id": "c92c8289-3590-4a85-b699-98bc8171ccde", "description": "Systematic review for CTG monitoring",
-                  "created": "2018-08-17T13:10:56.98669+01:00", "modified": "2018-08-17T13:10:57.023286+01:00",
-                  "earliest_start_date": "2018-09-15T13:00:00+01:00", "closing_date": "2018-08-17T13:00:00+01:00", "signup_status": "not-open",
-                  "task_type_id": "86118f6f-15e9-4c4b-970c-4c9f15c4baf6", "visibility": "public", "external_system_id": "e056e0bf-8d24-487e-a57b-4e812b40c4d8",
+                 {"id": "c92c8289-3590-4a85-b699-98bc8171ccde",
+                  "description": "Systematic review for CTG monitoring",
+                  "created": f"2018-08-17T{tz_hour}:10:56.98669+{tz_offset}",
+                  "modified": f"2018-08-17T{tz_hour}:10:57.023286+{tz_offset}",
+                  "earliest_start_date": f"2018-09-15T{tz_hour}:00:00+{tz_offset}",
+                  "closing_date": f"2018-08-17T{tz_hour}:00:00+{tz_offset}",
+                  "signup_status": "not-open",
+                  "task_type_id": "86118f6f-15e9-4c4b-970c-4c9f15c4baf6",
+                  "visibility": "public",
+                  "external_system_id": "e056e0bf-8d24-487e-a57b-4e812b40c4d8",
                   "external_task_id": "1234", "base_url": 'http://crowd.cochrane.org/index.html', "status": "active"},
                  {"id": "4ee70544-6797-4e21-8cec-5653c8d5b234", "description": "Midwife assessment for CTG monitoring",
-                  "created": "2018-08-17T13:10:57.074321+01:00", "modified": "2018-08-17T13:10:57.111495+01:00",
-                  "earliest_start_date": None, "closing_date": "2018-08-17T10:30:00+01:00", "signup_status": "open",
-                  "task_type_id": "d92d9935-cb9e-4422-9dbb-65c3423599b1", "visibility": "public", "external_system_id": "9645a6b1-febb-4c40-8ee4-ff3264cf26af",
+                  "created": f"2018-08-17T{tz_hour}:10:57.074321+{tz_offset}",
+                  "modified": f"2018-08-17T{tz_hour}:10:57.111495+{tz_offset}",
+                  "earliest_start_date": None,
+                  "closing_date": f"2018-08-17T{tz_closing_date_hour}:30:00+{tz_offset}",
+                  "signup_status": "open",
+                  "task_type_id": "d92d9935-cb9e-4422-9dbb-65c3423599b1", "visibility": "public",
+                  "external_system_id": "9645a6b1-febb-4c40-8ee4-ff3264cf26af",
                   "external_task_id": None, "base_url": "https://www.qualtrics.com", "status": "active"}]},
-            {"id": "0c137d9d-e087-448b-ba8d-24141b6ceecd", "name": "Ambulance equipment", "short_name": "Ambulance equipment",
-             "created": "2018-08-17T13:10:56.173198+01:00", "modified": "2018-08-17T13:10:56.209544+01:00", "status": "active",
+            {"id": "0c137d9d-e087-448b-ba8d-24141b6ceecd", "name": "Ambulance equipment",
+             "short_name": "Ambulance equipment",
+             "created": f"2018-08-17T{tz_hour}:10:56.173198+{tz_offset}",
+             "modified": f"2018-08-17T{tz_hour}:10:56.209544+{tz_offset}", "status": "active",
              "visibility":"private",
              "tasks": [
                  {"id": "6f1c63e2-fbe8-4d24-8680-c68a30b407e3", "description": "Systematic review for ambulance bag",
-                  "created": "2018-08-17T13:10:57.162016+01:00", "modified": "2018-08-17T13:10:57.198223+01:00",
-                  "earliest_start_date": "2018-11-12T20:00:00+00:00", "closing_date": None, "signup_status": "invitation",
-                  "task_type_id": "86118f6f-15e9-4c4b-970c-4c9f15c4baf6", "visibility": "private", "external_system_id": "e056e0bf-8d24-487e-a57b-4e812b40c4d8",
+                  "created": f"2018-08-17T{tz_hour}:10:57.162016+{tz_offset}",
+                  "modified": f"2018-08-17T{tz_hour}:10:57.198223+{tz_offset}",
+                  "earliest_start_date": "2018-11-12T20:00:00+00:00",
+                  "closing_date": None, "signup_status": "invitation",
+                  "task_type_id": "86118f6f-15e9-4c4b-970c-4c9f15c4baf6", "visibility": "private",
+                  "external_system_id": "e056e0bf-8d24-487e-a57b-4e812b40c4d8",
                   "external_task_id": "abcd", "base_url": "http://crowd.cochrane.org/index.html", "status": "active"},
                  {"id": "f3316529-e073-435e-b5c7-053da4127e96", "description": "Photos of ambulance equipment",
-                  "created": "2018-08-17T13:10:57.275273+01:00", "modified": "2018-08-17T13:10:57.311031+01:00",
+                  "created": f"2018-08-17T{tz_hour}:10:57.275273+{tz_offset}",
+                  "modified": f"2018-08-17T{tz_hour}:10:57.311031+{tz_offset}",
                   "earliest_start_date": None, "closing_date": None, "signup_status": "closed",
-                  "task_type_id": "a5537c85-7d29-4500-9986-ddc18b27d46f", "visibility": "private", "external_system_id": "9645a6b1-febb-4c40-8ee4-ff3264cf26af",
-                  "external_task_id": "8e368360-a708-4336-8feb-a8903fde0210", "base_url": 'https://www.qualtrics.com', "status": "complete"}]
+                  "task_type_id": "a5537c85-7d29-4500-9986-ddc18b27d46f", "visibility": "private",
+                  "external_system_id": "9645a6b1-febb-4c40-8ee4-ff3264cf26af",
+                  "external_task_id": "8e368360-a708-4336-8feb-a8903fde0210",
+                  "base_url": 'https://www.qualtrics.com', "status": "complete"}]
              }]
 
-        expected_body_gmt = [
-            {"id": "3ffc498f-8add-4448-b452-4fc7f463aa21", "name": "CTG Monitoring", "short_name": "CTG Monitoring",
-             "created": "2018-08-17T12:10:56.084487+00:00", "modified": "2018-08-17T12:10:56.119612+00:00", "status": "complete",
-             "visibility":"public",
-             "tasks": [
-                 {"id": "c92c8289-3590-4a85-b699-98bc8171ccde", "description": "Systematic review for CTG monitoring",
-                  "created": "2018-08-17T12:10:56.98669+00:00", "modified": "2018-08-17T12:10:57.023286+00:00",
-                  "earliest_start_date": "2018-09-15T12:00:00+00:00", "closing_date": "2018-08-17T12:00:00+00:00", "signup_status": "not-open",
-                  "task_type_id": "86118f6f-15e9-4c4b-970c-4c9f15c4baf6", "visibility": "public", "external_system_id": "e056e0bf-8d24-487e-a57b-4e812b40c4d8",
-                  "external_task_id": "1234", "base_url": 'http://crowd.cochrane.org/index.html', "status": "active"},
-                 {"id": "4ee70544-6797-4e21-8cec-5653c8d5b234", "description": "Midwife assessment for CTG monitoring",
-                  "created": "2018-08-17T12:10:57.074321+00:00", "modified": "2018-08-17T12:10:57.111495+00:00",
-                  "earliest_start_date": None, "closing_date": "2018-08-17T09:30:00+00:00", "signup_status": "open",
-                  "task_type_id": "d92d9935-cb9e-4422-9dbb-65c3423599b1", "visibility": "public", "external_system_id": "9645a6b1-febb-4c40-8ee4-ff3264cf26af",
-                  "external_task_id": None, "base_url": "https://www.qualtrics.com", "status": "active"}]},
-            {"id": "0c137d9d-e087-448b-ba8d-24141b6ceecd", "name": "Ambulance equipment", "short_name": "Ambulance equipment",
-             "created": "2018-08-17T12:10:56.173198+00:00", "modified": "2018-08-17T12:10:56.209544+00:00", "status": "active",
-             "visibility":"private",
-             "tasks": [
-                 {"id": "6f1c63e2-fbe8-4d24-8680-c68a30b407e3", "description": "Systematic review for ambulance bag",
-                  "created": "2018-08-17T12:10:57.162016+00:00", "modified": "2018-08-17T12:10:57.198223+00:00",
-                  "earliest_start_date": "2018-11-12T20:00:00+00:00", "closing_date": None, "signup_status": "invitation",
-                  "task_type_id": "86118f6f-15e9-4c4b-970c-4c9f15c4baf6", "visibility": "private", "external_system_id": "e056e0bf-8d24-487e-a57b-4e812b40c4d8",
-                  "external_task_id": "abcd", "base_url": "http://crowd.cochrane.org/index.html", "status": "active"},
-                 {"id": "f3316529-e073-435e-b5c7-053da4127e96", "description": "Photos of ambulance equipment",
-                  "created": "2018-08-17T12:10:57.275273+00:00", "modified": "2018-08-17T12:10:57.311031+00:00",
-                  "earliest_start_date": None, "closing_date": None, "signup_status": "closed",
-                  "task_type_id": "a5537c85-7d29-4500-9986-ddc18b27d46f", "visibility": "private", "external_system_id": "9645a6b1-febb-4c40-8ee4-ff3264cf26af",
-                  "external_task_id": "8e368360-a708-4336-8feb-a8903fde0210", "base_url": 'https://www.qualtrics.com', "status": "complete"}]
-             }]
-
-        expected_body = expected_body_gmt
         # result = list_projects_api(None, None)
         result = test_get(list_projects_api, ENTITY_BASE_URL, None, None, None)
         result_status = result['statusCode']
