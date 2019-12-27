165ebe8e9e429d29ebf18a0a67d8f2625075d7ca
André Sartori <afs25@cam.ac.uk>
2019-12-27 05:36:10 +0000

    #2876 comment: added function post_sample_users_to_crm to setup-sample-data.py and rename file to allow import as module

--- api/local/setup_sample_data.py
+++ api/local/setup_sample_data.py
@@ -15,11 +15,40 @@
 #   A copy of the GNU Affero General Public License is available in the
 #   docs folder of this project.  It is also available www.gnu.org/licenses/
 #
+import csv
+import os
 
+import api.common.hubspot as hs
+import api.endpoints.user as user
 from api.common.pg_utilities import insert_data_from_csv, truncate_table, populate_table_from_csv
-from api.common.utilities import get_aws_namespace
+from api.common.utilities import get_aws_namespace, get_country_name, now_with_tz
 
-TEST_DATA_FOLDER = '../tests/test_data/'
+# TEST_DATA_FOLDER = '../tests/test_data/'
+TEST_DATA_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'tests', 'test_data')
+USER_DATA_FILE = os.path.join(TEST_DATA_FOLDER, 'user_data_PSFU.csv')
+
+
+def post_sample_users_to_crm():
+    with open(USER_DATA_FILE) as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            user_json = {
+                "id": row[0],
+                "created": row[1],
+                "email": row[3],
+                "first_name": row[5],
+                "last_name": row[6],
+                "country_code": row[12],
+                "country_name": get_country_name(row[12]),
+                "avatar_string": f'{row[5][0].upper()}{row[6][0].upper()}',
+                "status": "new"
+            }
+
+            hubspot_id, _ = hs.post_new_user_to_crm(user_json, correlation_id=None)
+            user_jsonpatch = [
+                {'op': 'replace', 'path': '/crm_id', 'value': str(hubspot_id)},
+            ]
+            user.patch_user(user_json['id'], user_jsonpatch, now_with_tz(), correlation_id=None)
 
 
 def populate_database():
@@ -30,7 +59,7 @@ def populate_database():
     insert_data_from_csv(TEST_DATA_FOLDER + 'projecttask_data_PSFU.csv', 'public.projects_projecttask')
     insert_data_from_csv(TEST_DATA_FOLDER + 'projectgroupvisibility_data.csv', 'public.projects_projectgroupvisibility')
     insert_data_from_csv(TEST_DATA_FOLDER + 'projecttaskgroupvisibility_data.csv', 'public.projects_projecttaskgroupvisibility')
-    insert_data_from_csv(TEST_DATA_FOLDER + 'user_data_PSFU.csv', 'public.projects_user')
+    insert_data_from_csv(USER_DATA_FILE, 'public.projects_user')
     insert_data_from_csv(TEST_DATA_FOLDER + 'usergroupmembership_data.csv', 'public.projects_usergroupmembership')
     insert_data_from_csv(TEST_DATA_FOLDER + 'userproject_PSFU.csv', 'public.projects_userproject')
     insert_data_from_csv(TEST_DATA_FOLDER + 'usertask_PSFU.csv', 'public.projects_usertask')
@@ -79,6 +108,7 @@ if __name__ == "__main__":
         # pass
         # clear_database()
         populate_database()
+        post_sample_users_to_crm()
         # populate_database_london_dev()
     else:
         print ('Action cancelled')
