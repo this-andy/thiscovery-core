89ec6f61078ec541a5816ca26177bb13b0b6b167
André Sartori <afs25@cam.ac.uk>
2020-01-16 16:50:42 +0000

    #2879 comment: added try statement for queryStringParameters in progress_process.py

--- api/endpoints/progress_process.py
+++ api/endpoints/progress_process.py
@@ -51,8 +51,11 @@ def update_cochrane_progress(event, context):
     correlation_id = get_correlation_id(event)
 
     # optional base_url queryStringParameter so that tests can always point to mock API
-    params = event.get('queryStringParameters')
-    api_url = params.get('api_url')
+    try:
+        params = event['queryStringParameters']
+        api_url = params.get('api_url')
+    except KeyError:
+        api_url = None
 
     if api_url:
         progress_dict = get_progress(api_url=api_url)
