d221493037a06cf0c03a32cdf3f04b1bb1c0cb68
André Sartori <afs25@cam.ac.uk>
2020-02-07 10:10:41 +0000

    #2896 comment: experimenting with using the models in thiscovery_admin to facilitate sql management

--- api/local/admin_tasks/general_admin/generate_table_classes_for_sql_queries.py
+++ api/local/admin_tasks/general_admin/generate_table_classes_for_sql_queries.py
@@ -4,22 +4,13 @@ sys.path.append(PATH_TO_THISCOVERY_ADMIN)
 
 import django
 from django.conf import settings
-from thiscovery_admin.settings import test
+from thiscovery_admin.settings import base
+from django.conf.global_settings import *
 
-settings.configure(default_settings=test, DEBUG=True)
+settings.configure(default_settings=django.conf.global_settings, DEBUG=True)
 django.setup()
 
 from thiscovery_admin.projects import models
-
-user1 = models.User.objects.get(id='d1070e81-557e-40eb-a7ba-b951ddb7ebdc')
-user1_projects = user1.user_project.all()
-print(user1)
-print(user1_projects)
-
-user1_projects_in_one_query = models.User.objects.get(id='d1070e81-557e-40eb-a7ba-b951ddb7ebdc').user_project.all()
-print('user1_projects_in_one_query:', user1_projects_in_one_query)
-
-print('\nuser1_projects_in_one_query SQL:\n\t', user1_projects_in_one_query.query)
 
 user = models.User()
 
