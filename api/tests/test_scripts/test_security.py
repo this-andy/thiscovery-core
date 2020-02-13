29fffc4f34154df626197a733fad3c677e473a0f
André Sartori <afs25@cam.ac.uk>
2020-02-13 13:26:45 +0000

    #2917 comment: added test of get_user_by_id endpoint

--- api/tests/test_scripts/test_security.py
+++ api/tests/test_scripts/test_security.py
@@ -44,17 +44,14 @@ class TestApiEndpoints(test_utils.BaseTestCase):
     def setUpClass(cls):
         os.environ['TEST_ON_AWS'] = 'true'
 
-    def test_01_get_user_by_uuid_api_exists(self):
+    def test_01_get_user_by_uuid_api_requires_valid_key(self):
+        blank_api_key = ''
         invalid_api_key = '3c907908-44a7-490a-9661-3866b3732d22'
         path_parameters = {'id': "d1070e81-557e-40eb-a7ba-b951ddb7ebdc"}
 
-        # expected_status = HTTPStatus.OK
+        expected_status = HTTPStatus.FORBIDDEN
 
-        result = test_get(get_user_by_id_api, 'v1/user', path_parameters=path_parameters, aws_api_key=invalid_api_key)
-        print(result)
-        # result_status = result['statusCode']
-        # result_json = json.loads(result['body'])
-        #
-        # # test results returned from api call
-        # self.assertEqual(expected_status, result_status)
-        # self.assertDictEqual(EXPECTED_USER, result_json)
+        for key in [blank_api_key, invalid_api_key]:
+            result = test_get(get_user_by_id_api, 'v1/user', path_parameters=path_parameters, aws_api_key=key)
+            result_status = result['statusCode']
+            self.assertEqual(expected_status, result_status)
