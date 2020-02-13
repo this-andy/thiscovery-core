4471717e6c39746c6a1827e918d3c9380dd19361
André Sartori <afs25@cam.ac.uk>
2020-02-13 16:12:17 +0000

    #2917 comment: wrote tests for all api endpoints; deleted handler function of dateformattest api endpoint

--- api/tests/test_scripts/test_security.py
+++ api/tests/test_scripts/test_security.py
@@ -27,6 +27,10 @@ import api.endpoints.misc as m
 import api.endpoints.notification_process as np
 import api.endpoints.project as p
 import api.endpoints.user as u
+import api.endpoints.user_external_account as uea
+import api.endpoints.user_group_membership as ugm
+import api.endpoints.user_project as up
+import api.endpoints.user_task as ut
 import common.utilities as utils
 import testing_utilities as test_utils
 
@@ -68,7 +72,7 @@ class TestUserApiEndpoints(TestApiEndpoints):
 
     def test_02_get_user_email_api_requires_valid_key(self):
         querystring_parameters = {'email': 'altha@email.co.uk'}
-        self.check_api_is_restricted('GET', u.get_user_by_id_api, 'v1/user', querystring_parameters=querystring_parameters)
+        self.check_api_is_restricted('GET', u.get_user_by_id_api, self.ENTITY_BASE_URL, querystring_parameters=querystring_parameters)
 
     def test_03_patch_user_api_requires_valid_key(self):
         path_parameters = {'id': "d1070e81-557e-40eb-a7ba-b951ddb7ebdc"}
@@ -104,8 +108,86 @@ class TestProjectApiEndpoints(TestApiEndpoints):
         path_parameters = {'id': "a099d03b-11e3-424c-9e97-d1c095f9823b"}
         self.check_api_is_restricted('GET', p.get_project_api, f'v1/{self.ENTITY_BASE_URL}', path_parameters=path_parameters)
 
+    def test_07_get_project_statuses_api_requires_valid_key(self):
+        querystring_parameters = {'user_id': 'd1070e81-557e-40eb-a7ba-b951ddb7ebdc'}
+        version_mapping = {
+            'v1': p.get_project_status_for_user_api,
+            'v2': p.get_project_status_for_external_user_api,
+        }
+        for api_version, handler_function in version_mapping.items():
+            self.check_api_is_restricted('GET', handler_function, f'{api_version}/project-user-status', querystring_parameters=querystring_parameters)
+
 
 class TestMiscApiEndpoints(TestApiEndpoints):
 
-    def test_01_ping_is_public(self):
+    def test_08_ping_is_public(self):
         self.check_api_is_public('GET', m.ping, 'v1/ping')
+
+    def test_09_raiseerror_is_public(self):
+        querystring_parameters = {'error_id': '200'}
+        self.check_api_is_public('POST', m.raise_error_api, 'v1/raise-error', querystring_parameters=querystring_parameters)
+
+
+class TestUserGroupMembershipApiEndpoints(TestApiEndpoints):
+    ENTITY_BASE_URL = 'v1/usergroupmembership'
+
+    def test_10_create_user_group_membership_api_requires_valid_key(self):
+        body = json.dumps({
+            'user_id': '1cbe9aad-b29f-46b5-920e-b4c496d42515',
+            'user_group_id': 'de1192a0-bce9-4a74-b177-2a209c8deeb4',
+        })
+        self.check_api_is_restricted('POST', ugm.create_user_group_membership_api, self.ENTITY_BASE_URL, request_body=body)
+
+
+class TestUserProjectApiEndpoints(TestApiEndpoints):
+    ENTITY_BASE_URL = 'v1/userproject'
+
+    def test_11_list_user_projects_api_requires_valid_key(self):
+        querystring_parameters = {'user_id': '851f7b34-f76c-49de-a382-7e4089b744e2'}
+        self.check_api_is_restricted('GET', up.list_user_projects_api, self.ENTITY_BASE_URL, querystring_parameters=querystring_parameters)
+
+    def test_12_create_user_projects_api_requires_valid_key(self):
+        body = json.dumps({
+            'user_id': "35224bd5-f8a8-41f6-8502-f96e12d6ddde",
+            'project_id': "5907275b-6d75-4ec0-ada8-5854b44fb955",
+            'ext_user_project_id': 'b75c864b-a002-466c-989f-16f63d5a6b18',
+            'status': 'active',
+            'id': '9620089b-e9a4-46fd-bb78-091c8449d777',
+            'created': '2018-06-13 14:15:16.171819+00'
+        })
+        self.check_api_is_restricted('POST', up.create_user_project_api, self.ENTITY_BASE_URL, request_body=body)
+
+
+class TestUserTaskApiEndpoints(TestApiEndpoints):
+    ENTITY_BASE_URL = 'v1/usertask'
+
+    def test_13_list_user_tasks_api_requires_valid_key(self):
+        querystring_parameters = {'user_id': '851f7b34-f76c-49de-a382-7e4089b744e2'}
+        self.check_api_is_restricted('GET', ut.list_user_tasks_api, self.ENTITY_BASE_URL, querystring_parameters=querystring_parameters)
+
+    def test_14_create_user_task_api_requires_valid_key(self):
+        body = json.dumps({
+            'user_id': '48e30e54-b4fc-4303-963f-2943dda2b139',
+            'project_task_id': '6cf2f34e-e73f-40b1-99a1-d06c1f24381a',
+            'ext_user_task_id': '78a1ccd7-dee5-49b2-ad5c-8bf4afb3cf93',
+            'status': 'active',
+            'consented': '2018-06-12 16:16:56.087895+01',
+            'id': '9620089b-e9a4-46fd-bb78-091c8449d777',
+            'created': '2018-06-13 14:15:16.171819+00'
+        })
+        self.check_api_is_restricted('POST', ut.create_user_task_api, self.ENTITY_BASE_URL, request_body=body)
+
+
+class TestUserExternalAccountApiEndpoints(TestApiEndpoints):
+    ENTITY_BASE_URL = 'v1/userexternalaccount'
+
+    def test_15_create_user_external_account_api_requires_valid_key(self):
+        body = json.dumps({
+            'external_system_id': "e056e0bf-8d24-487e-a57b-4e812b40c4d8",
+            'user_id': "35224bd5-f8a8-41f6-8502-f96e12d6ddde",
+            'external_user_id': 'cc02',
+            'status': 'active',
+            'id': '9620089b-e9a4-46fd-bb78-091c8449d777',
+            'created': '2018-06-13 14:15:16.171819+00'
+        })
+        self.check_api_is_restricted('POST', uea.create_user_external_account_api, self.ENTITY_BASE_URL, request_body=body)
