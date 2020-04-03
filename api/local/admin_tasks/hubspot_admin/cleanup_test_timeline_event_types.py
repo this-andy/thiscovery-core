75479f1d379cd3e012de37491728033f1cdf3d1c
André Sartori <afs25@cam.ac.uk>
2020-04-03 15:41:10 +0100

    #2935 comment: added cleanup_test_timeline_event_types.py and list_timeline_event_types method

---
+++ api/local/admin_tasks/hubspot_admin/cleanup_test_timeline_event_types.py
@@ -0,0 +1,29 @@
+#
+#   Thiscovery API - THIS Institute’s citizen science platform
+#   Copyright (C) 2019 THIS Institute
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   A copy of the GNU Affero General Public License is available in the
+#   docs folder of this project.  It is also available www.gnu.org/licenses/
+#
+"""
+This script fetches all timeline event types associated with an environment and deletes all event types whose name == "Test timeline type"
+"""
+from api.common.hubspot import HubSpotClient
+
+if __name__ == "__main__":
+    hs_client = HubSpotClient()
+    ets = hs_client.list_timeline_event_types()
+    for et in ets:
+        if et['name'] == "Test timeline type":
+            response = hs_client.delete_timeline_event_type(et['id'])
+            assert response == 204, f"HubSpot API returned an unexpected response: {response}"
